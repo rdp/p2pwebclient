4c81c323113d76f20b85db285585b4d2253a3a4b
rogerdpack <rogerpack2005@gmail.com>
2009-06-29 11:50:39 -0600

    some changes to writeup

--- documents/writeup/final/go.sh
+++ documents/writeup/final/go.sh
@@ -1,11 +1,12 @@
 ruby ruby_to_tex.rb experiment_results.tex.erb 
 
 # note -- the first pass it can't get the ref's right! 
-pdflatex thesis.tex
+
+# pdflatex thesis.tex
+# bibtex thesis
 # pdflatex thesis.tex
 pdflatex just_experiment_results.tex
 # pdflatex just_experiment_results.tex & >/dev/null
-#bibtex thesis
 chmod a+r *.pdf
 ruby ruby_to_tex.rb experiment_results.tex.erb  # in case it failed
 rsync *.pdf wilkboar@wilkboardonline.com:~/public_html/roger/p2p/writeup & >/dev/null
