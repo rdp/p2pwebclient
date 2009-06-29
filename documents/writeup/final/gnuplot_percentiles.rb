bedca42aa43ebfe2154a6873dc031fa0863e7187
rogerdpack <rogerpack2005@gmail.com>
2009-06-29 16:36:07 -0600

    some renames, retain logs too

--- documents/writeup/final/gnuplot_percentiles.rb
+++ documents/writeup/final/gnuplot_percentiles.rb
@@ -1,14 +1,21 @@
-# 1	1.5	2 	2.4	4	6.
-# 2	1.5	3 	3.5	4	5.5
-# 3	4.5	5 	5.5	6	6.5
-a = [1,2,3]
-b = [1.5, 1.5, 4.5]
-c = [2,3,5]
-d = [2.4, 3.5, 5.5]
-e = [4,4,6]
-f = [6,5.5, 6.5] 
+
+# so gnuplot expected something like
+# x    1st     25th    50th   75th    99th
+# 1    1.5     2       2.4     4       6.
+# 2    1.5     3       3.5     4       5.5
+# 3    4.5     5       5.5     6       6.5
+#x = [1,2,3]
+#b = [1.5, 1.5, 4.5]
+#c = [2,3,5]
+#d = [2.4, 3.5, 5.5]
+#e = [4,4,6]
+#f = [6,5.5, 6.5]
+
+numbers = [[799.98, 1330.81, 1604.27, 1945.76, 2555.93], [833.85, 1115.37, 1228.83, 1416.25, 1867.09], [919.66, 1054.67, 1123.94, 1249.02, 1707.25], [773.26, 905.26, 972.88, 1080.27, 1634.38], [247.47, 889.17, 931.92, 1002.24, 1266.23], [810.28, 869.11, 901.46, 986.36, 1355.63], [739.22, 834.95, 876.03, 926.65, 1636.65]]
+
 require 'rubygems'
 require 'gnuplot'
+
 Gnuplot.open do |gp|
   Gnuplot::Plot.new( gp ) do |plot|
   
@@ -18,13 +25,14 @@ Gnuplot.open do |gp|
     plot.xrange "[0:11]"
     plot.yrange "[0:10]"
 
-    plot.data << Gnuplot::DataSet.new( [a,b,c,d,e,f] ) do |ds|
+    plot.data << Gnuplot::DataSet.new( [x,b,c,d,e,f] ) do |ds|
       ds.using = "1:3:2:6:5"
       ds.with = "candlesticks lt 3 lw 2 title 'Quartiles' whiskerbars"
       #ds.notitle
     end
 
-    plot.data << Gnuplot::DataSet.new( [a,b,c,d,e,f] ) do |ds|
+    # add median...kind of...
+    plot.data << Gnuplot::DataSet.new( [x,b,c,d,e,f] ) do |ds|
       ds.using = "1:4:4:4:4"
       ds.with = "candlesticks lt -1 lw 2"
       ds.notitle
