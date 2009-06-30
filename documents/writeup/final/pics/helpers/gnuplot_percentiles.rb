d5790dc0f41dbd152f7ca8f9c977997688562a95
rogerdpack <rogerpack2005@gmail.com>
2009-06-29 18:32:53 -0600

    working regrapher, I think

--- documents/writeup/final/pics/helpers/gnuplot_percentiles.rb
+++ documents/writeup/final/pics/helpers/gnuplot_percentiles.rb
@@ -1,9 +1,9 @@
 # expected something like [filename, x axis, y axis]
 # and parse some percentile graphs
-stat = File.read 'number_stats.txt'
-numbers = []
-stats.each_line{|line| line =~ /just numbers.*?_at(\d+\.\d+|\d+)/; number << $1 $1 }
-puts numbers.inspect
+stats = File.read 'number_stats.txt'
+xs = []
+stats.each_line{|line| line =~ /just numbers.*?_at(\d+\.\d+|\d+)/; xs << $1 if $1 }
+puts xs.inspect
 
 
 
@@ -19,8 +19,21 @@ puts numbers.inspect
 #e = [4,4,6]
 #f = [6,5.5, 6.5]
 
-x =  numbers
-numbers = [[799.98, 1330.81, 1604.27, 1945.76, 2555.93], [833.85, 1115.37, 1228.83, 1416.25, 1867.09], [919.66, 1054.67, 1123.94, 1249.02, 1707.25], [773.26, 905.26, 972.88, 1080.27, 1634.38], [247.47, 889.17, 931.92, 1002.24, 1266.23], [810.28, 869.11, 901.46, 986.36, 1355.63], [739.22, 834.95, 876.03, 926.65, 1636.65]]
+
+readings = eval(File.read(ARGV[0]))
+# something like [[0.0, 0.0, 0.0, 118003.5, 1200130.5], [0.0, 0.0, 0.0, 319402.5, 1112664.0], [0.0, 0.0, 0.0, 105941.5, 924438.5], [0.0, 0.0, 10860.0, 161322.0, 708114.5], [0.0, 0.0, 10518.0, 159478.5, 679632.0], [0.0, 0.0, 0.0, 171059.5, 731339.0], [0.0, 0.0, 0.0, 5672.0, 786340.5], [0.0, 0.0, 0.0, 79341.0, 966801.0], [0.0, 0.0, 1448.0, 189178.5, 809426.0]]
+
+locations = {1 => 0, 25 => 1, 50 => 2, 75 => 3, 99 => 4}
+percentiles = []
+
+for percentile, location in locations.sort
+  new = []
+  for reading in readings
+    new << reading[location]
+  end
+  percentiles << new
+end
+puts "percentiles are", percentiles.inspect, xs.inspect
 
 require 'rubygems'
 require 'gnuplot'
@@ -29,25 +42,23 @@ Gnuplot.open do |gp|
   Gnuplot::Plot.new( gp ) do |plot|
   
     plot.title  "Example"
-    plot.ylabel ARGV[1]
-    plot.xlabel ARGV[2]
+    plot.ylabel ARGV[1] if ARGV[1]
+    plot.xlabel ARGV[2] if ARGV[2]
 #    plot.xrange "[0:11]" # TODO
 #    plot.yrange "[0:10]"
 
-    plot.data << Gnuplot::DataSet.new( [x,b,c,d,e,f] ) do |ds|
+    plot.data << Gnuplot::DataSet.new( [xs] + percentiles ) do |ds|
       ds.using = "1:3:2:6:5"
       ds.with = "candlesticks lt 3 lw 2 title 'Quartiles' whiskerbars"
       #ds.notitle
     end
 
     # add median...kind of...
-    plot.data << Gnuplot::DataSet.new( [x,b,c,d,e,f] ) do |ds|
+    plot.data << Gnuplot::DataSet.new( [xs] + percentiles ) do |ds|
       ds.using = "1:4:4:4:4"
       ds.with = "candlesticks lt -1 lw 2"
       ds.notitle
     end
-    #require 'ruby-debug'
-    #debugger
 
   end
 end
