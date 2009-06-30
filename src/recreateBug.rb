7bc74ec04e3d06a0c9a1cf30b0499e8b24cc7d16
rogerdpack <rogerpack2005@gmail.com>
2009-06-29 18:04:27 -0600

    add writeup comments

--- src/recreateBug.rb
+++
@@ -1,9 +0,0 @@
-require 'constants.rb'
-require 'server_slow_peer.rb'
-require 'listener.rb'
-require 'driver.rb'
-
-# ltodo in fakepeerserver change it to say serverPort
-count = ARGV.shift
-count ||= 50
-Driver.recreateBug count.to_i
