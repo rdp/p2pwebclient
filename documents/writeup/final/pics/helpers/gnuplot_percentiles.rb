beaec9cb5cb7f87eb5dff21b7cc239e86b222c49
rogerdpack <rogerpack2005@gmail.com>
2009-07-03 19:24:52 -0600

    prettier

--- documents/writeup/final/pics/helpers/gnuplot_percentiles.rb
+++ documents/writeup/final/pics/helpers/gnuplot_percentiles.rb
@@ -15,53 +15,72 @@
 
 
 def do_file filename, ylabel = nil, xlabel = nil
-# expected something like [filename, x axis, y axis]
-# and parse some percentile graphs
-stats = File.read 'number_stats.txt'
-xs = []
-stats.each_line{|line| line =~ /just numbers.*?_at(\d+\.\d+|\d+)/; xs << $1 if $1 }
-readings = eval(File.read(filename))
-# starts like [[0.0, 0.0, 0.0, 118003.5, 1200130.5], [0.0, 0.0, 0.0, 319402.5, 1112664.0], [0.0, 0.0, 0.0, 105941.5, 924438.5], [0.0, 0.0, 10860.0, 161322.0, 708114.5], [0.0, 0.0, 10518.0, 159478.5, 679632.0], [0.0, 0.0, 0.0, 171059.5, 731339.0], [0.0, 0.0, 0.0, 5672.0, 786340.5], [0.0, 0.0, 0.0, 79341.0, 966801.0], [0.0, 0.0, 1448.0, 189178.5, 809426.0]]
-locations = {1 => 0, 25 => 1, 50 => 2, 75 => 3, 99 => 4}
-percentiles = []
+   # expected something like [filename, x axis, y axis]
+   # and parse some percentile graphs
+   stats = File.read 'number_stats.txt'
+   xs = []
+   stats.each_line{|line| line =~ /just numbers.*?_at(\d+\.\d+|\d+)/; xs << $1 if $1 }
+   readings = eval(File.read(filename))
+   # comes to us like [[0.0, 0.0, 0.0, 118003.5, 1200130.5], [0.0, 0.0, 0.0, 319402.5, 1112664.0], [0.0, 0.0, 0.0, 105941.5, 924438.5], [0.0, 0.0, 10860.0, 161322.0, 708114.5], [0.0, 0.0, 10518.0, 159478.5, 679632.0], [0.0, 0.0, 0.0, 171059.5, 731339.0], [0.0, 0.0, 0.0, 5672.0, 786340.5], [0.0, 0.0, 0.0, 79341.0, 966801.0], [0.0, 0.0, 1448.0, 189178.5, 809426.0]]
 
-for percentile, location in locations.sort
-  new = []
-  for reading in readings
-    new << reading[location]
-  end
-  percentiles << new
+   # parse it out to rgnuplot style arrays...
+   locations = {1 => 0, 25 => 1, 50 => 2, 75 => 3, 99 => 4}
+   percentiles = []
+
+   for percentile, location in locations.sort
+      new = []
+      for reading in readings
+         new << reading[location]
+      end
+      percentiles << new
+   end
+   puts "percentiles are", percentiles.inspect, xs.inspect
+
+   require 'rubygems'
+   require 'gnuplot'
+
+   Gnuplot.open do |gp|
+      Gnuplot::Plot.new( gp ) do |plot|
+
+         plot.title  "Example"
+         plot.ylabel ylabel if ylabel
+         plot.xlabel xlabel if xlabel
+         #    plot.xrange "[0:11]"
+         #    plot.yrange "[0:10]"
+
+         plot.data << Gnuplot::DataSet.new( [xs] + percentiles ) do |ds|
+            ds.using = "1:3:2:6:5"
+            # ugly blue      ds.with = "candlesticks lt 3 lw 2 title '1,25,50,75,99th percentiles' "
+            ds.with = "candlesticks title '1,25,50,75,99th percentiles' "
+            #ds.notitle
+         end
+
+         # add the median...kind of...since all it is is a line
+         plot.data << Gnuplot::DataSet.new( [xs] + percentiles ) do |ds|
+            ds.using = "1:4:4:4:4"
+            ds.with = "candlesticks lt -1"
+            #      ds.with = "candlesticks"
+            ds.notitle
+         end
+
+      end
+   end
 end
-puts "percentiles are", percentiles.inspect, xs.inspect
-
 require 'rubygems'
-require 'gnuplot'
+require 'optiflag'
+if $0 == __FILE__
+   module DBChecker extend OptiFlagSet
+      optional_flag "file"
+      optional_flag "dir"
 
-Gnuplot.open do |gp|
-  Gnuplot::Plot.new( gp ) do |plot|
-  
-    plot.title  "Example"
-    plot.ylabel ylabel if ylabel
-    plot.xlabel xlabel if xlabel
-#    plot.xrange "[0:11]"
-#    plot.yrange "[0:10]"
+      and_process!
+   end
 
-    plot.data << Gnuplot::DataSet.new( [xs] + percentiles ) do |ds|
-      ds.using = "1:3:2:6:5"
-# ugly blue      ds.with = "candlesticks lt 3 lw 2 title '1,25,50,75,99th percentiles' "
-      ds.with = "candlesticks title '1,25,50,75,99th percentiles' "
-      #ds.notitle
-    end
+   do_file ARGV.flags.file if ARGV.flags.file
+   puts 'flags', ARGV.flags 
+   if ARGV.flags.dir
+     files = Dir.glob ARGV.flags.dir + '/*'
+     puts 'files', files.inspect
+   end
 
-    # add median...kind of...
-    plot.data << Gnuplot::DataSet.new( [xs] + percentiles ) do |ds|
-      ds.using = "1:4:4:4:4"
-      ds.with = "candlesticks lt -1"
-#      ds.with = "candlesticks"
-      ds.notitle
-    end
-
-  end
 end
-end
-do_file ARGV[0]
