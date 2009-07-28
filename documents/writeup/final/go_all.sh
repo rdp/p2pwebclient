23cf4e7c511326ad1dd37ff650889756774282d8
unknown <rogerpack2005@gmail.com>
2009-07-28 07:22:37 -0600

    update related works--feels more sane now

--- documents/writeup/final/go_all.sh
+++ documents/writeup/final/go_all.sh
@@ -3,9 +3,9 @@ ruby ruby_to_tex.rb experiment_results.tex.erb
 # note -- the first pass it can't get the ref's right! 
 
 pdflatex thesis.tex
-pdflatex thesis.tex & >/dev/null
+#pdflatex thesis.tex & >/dev/null
 pdflatex just_experiment_results.tex
-pdflatex just_experiment_results.tex & >/dev/null
+#pdflatex just_experiment_results.tex & >/dev/null
 pdflatex just_real_future_work.tex # fer fun
 bibtex thesis
 chmod a+r *.pdf
