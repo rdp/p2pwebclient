189b508e03ec403d0284e0e40022ae3203dd4fd0
Roger Pack <rogerpack2005@gmail.com>
2009-10-26 12:53:34 -0600

    update script

--- distro/superstart.sh
+++ distro/superstart.sh
@@ -1,2 +1,2 @@
-python vxargs-0.3.3.py --max-procs=200 -y --timeout=60 -a planetlab_hosts.txt -o vxOutForRe ssh byu_p2p@{}  "p2pwebclient/src/superstart_here.sh"
+python vxargs-0.3.3.py --max-procs=200 -y --timeout=60 -a planetlab_hosts.txt -o vxOutForRe ssh byu_p2p@{} ". p2pwebclient/src/setup_env_planetlab_host.sh; p2pwebclient/src/superstart_here.sh"
 
--- src/setup_env_planetlab_host.sh
+++ src/setup_env_planetlab_host.sh
@@ -1,4 +1,4 @@
-export PATH=/home/byu_p2pweb/i386/bin:/home/byu_p2pweb/i386/jre1.6.0_07/bin:$PATH
+export PATH=/home/byu_p2p/i386/bin:/home/byu_p2p/i386/jre1.6.0_07/bin:$PATH
 mkdir -p ../logs/`ruby -e "require 'lib/ruby_useful_here.rb'; print Socket.get_host_ip()"`/
 export LD_LIBRARY_PATH=./ext/planetlab_ilab # useful anymore?
 export INLINEDIR='./ext/planetlab_ilab'
