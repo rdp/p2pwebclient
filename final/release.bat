44c481dddd748d530613ee09612e8b71577fd1fa
rdp <rogerpack2005@gmail.com>
2010-03-09 14:52:52 -0700

    add pic

--- final/release.bat
+++ final/release.bat
@@ -1,4 +1,6 @@
 call go_all
+call go_all
+call go_all
 call go
 scp thesis.pdf wilkboar@wilkboardonline.com:~/public_html/roger/p2p/writeup
 scp just*.pdf wilkboar@wilkboardonline.com:~/public_html/roger/p2p/writeup
