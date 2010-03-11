4ea6b3c00e2747fb8cca175954b569041a543c30
rdp <rogerpack2005@gmail.com>
2010-03-10 19:21:25 -0700

    update scripts

--- final/release.bat
+++ final/release.bat
@@ -2,6 +2,7 @@ call go_all
 call go_all
 call go_all
 call go
+call go
 scp thesis.pdf wilkboar@wilkboardonline.com:~/public_html/roger/p2p/writeup
 scp just*.pdf wilkboar@wilkboardonline.com:~/public_html/roger/p2p/writeup
-
+beep
