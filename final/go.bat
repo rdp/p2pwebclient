d3f3342035ef9846f803041ff91986cc38290cb3
rogerdpack <rogerpack2005@gmail.com>
2010-03-26 17:00:01 -0600

    attempt to allow just experiment results to build from scratch

--- final/go.bat
+++ final/go.bat
@@ -9,6 +9,8 @@ pdflatex just_experiment_results.tex
 @rem # pdflatex just_experiment_results.tex & >/dev/null
 chmod a+r \*.pdf
 
+@rem in case it isn't there
+bibtex just_experiment_results
 @rem #scp just_experiment_results.pdf wilkboar@wilkboardonline.com:~/public_html/roger/p2p/writeup
 @cp just_experiment_results.pdf just_experiment_results_view_copy.pdf
 @chrome just_experiment_results_view_copy.pdf
